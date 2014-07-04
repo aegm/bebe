f9ca922ca341203f1087b689fe9f6a697ab2ae25
angel <angeledugo@hotmail.com>
2014-07-04 09:36:58 -0430

    carga inicial

---
+++ Ejemplo Tienda Online/includes/cabeceraadmin.php
@@ -0,0 +1,4 @@
+<p>Administracion</p>
+<p> <a href="../admin/productos_lista.php">Lista Productos</a></p>
+<p> <a href="../admin/categorias_lista.php">Lista Categorias</a></p>
+<p> <a href="../admin/usuarios_lista.php">Lista Usuarios</a></p>
